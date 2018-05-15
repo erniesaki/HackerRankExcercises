6e26b9fd5b0d5591033d96faf5c312d76f85e884
erniesaki <renzsaquilayan@homail.com>
2018-05-15 19:31:42 +0100

    first commit

--- src/HackerRank1.java
+++ src/HackerRank1.java
@@ -9,22 +9,25 @@ public class HackerRank1 {
         System.out.println("Hello, World.");
         System.out.println("Hello, Java.");
     }
+
     public void scanner() {
         Scanner scanner = new Scanner(System.in);
         int inpt1 = scanner.nextInt();
         int inpt2 = scanner.nextInt();
         int inpt3 = scanner.nextInt();
-        System.out.println(inpt1+"\n"+inpt2+"\n"+inpt3);
+        System.out.println(inpt1 + "\n" + inpt2 + "\n" + inpt3);
     }
+
     public void statement1() {
         Scanner scanner = new Scanner(System.in);
         int inpt1 = scanner.nextInt();
-        if(inpt1%2==1 || ((inpt1%2==0) && (inpt1>=6 && inpt1 <=20))) {
+        if (inpt1 % 2 == 1 || ((inpt1 % 2 == 0) && (inpt1 >= 6 && inpt1 <= 20))) {
             System.out.println("Weird");
         } else {
             System.out.println("Not Weird");
         }
     }
+
     public void scanner2() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a string");
@@ -33,32 +36,36 @@ public class HackerRank1 {
         double input2 = scanner.nextDouble();
         System.out.println("Enter an integer");
         int input3 = scanner.nextInt();
-        System.out.println("String: "+input1+"\n"+"Double: "+input2+"\n"+"Integer: "+input3);
+        System.out.println("String: " + input1 + "\n" + "Double: " + input2 + "\n" + "Integer: " + input3);
     }
+
     public void formating() {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("================================");
-        for(int counter=0;counter<3;counter++) {
-            String inpt1=scanner.next();
-            int inpt2=scanner.nextInt();
+        for (int counter = 0; counter < 3; counter++) {
+            String inpt1 = scanner.next();
+            int inpt2 = scanner.nextInt();
             System.out.printf("%-15s%03d%n", inpt1, inpt2);
         }
         System.out.println("================================");
     }
+
     public void loop1() {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         int inpt1 = scanner.nextInt();
-        for(int counter=1;counter<=10;counter++) {
-            System.out.println(inpt1 +" x "+ counter + " = " + inpt1*counter);
+        for (int counter = 1; counter <= 10; counter++) {
+            System.out.println(inpt1 + " x " + counter + " = " + inpt1 * counter);
         }
     }
+
     public void loop2() {
 
     }
-    public void primitive(){
-        Scanner scanner=new Scanner(System.in);
+
+    public void primitive() {
+        Scanner scanner = new Scanner(System.in);
         int inpt1 = scanner.nextInt();
-        for(int a=0;a<inpt1;a++) {
+        for (int a = 0; a < inpt1; a++) {
             try {
                 long inpt2 = scanner.nextLong();
                 System.out.println(inpt2 + " this can be fitted in:");
@@ -74,9 +81,17 @@ public class HackerRank1 {
                 if (inpt2 >= -Math.pow(2, 63) && inpt2 <= Math.pow(2, 63) - 1) {
                     System.out.println("* long");
                 }
-                  }catch(Exception e){
-                    System.out.println(scanner.next() + " can't be fitted anywhere.");
-                }
+            } catch (Exception e) {
+                System.out.println(scanner.next() + " can't be fitted anywhere.");
             }
         }
     }
+
+    public void endOfFile() {
+        Scanner scanner = new Scanner(System.in);
+        int inpt1 = scanner.nextInt();
+        int a = 0;
+        while (scanner.hasNext()) {
+            System.out.println(a + " " + scanner.nextLine() + a++);
+        }
+    } }
